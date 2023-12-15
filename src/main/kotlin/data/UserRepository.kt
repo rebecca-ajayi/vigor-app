f5a5ac73112e32622272a554a811842acac1de44
Becky-AJ <68607075+Becky-AJ@users.noreply.github.com>
2023-12-15 13:10:31 +0000

    C8: User repository handling interfaces (retrieve accounts and create users).

--- src/main/kotlin/data/UserRepository.kt
+++ src/main/kotlin/data/UserRepository.kt
@@ -1,4 +1,23 @@
 package data
 
-class UserRepository {
+import org.h2.engine.User
+
+interface UserRepository {
+    fun getUserById(id: String): User?
+    suspend fun createUser(user: User): User
+}
+
+object InMemoryUserRepo: UserRepository {
+
+    private val users = mutableListOf<User>()
+
+    override fun getUserById(id: String): User? {
+        return users.find { it.id.toString() == id }
+    }
+
+    override suspend fun createUser(user: User): User {
+        users.add(user)
+        return user
+    }
+
 }
